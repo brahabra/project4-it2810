e6ddcac0fe012b04888fabc276c306b30f38a0e5
bragehb <bragehb@stud.ntnu.no>
2022-11-13 16:04:49 +0100

    #12 Added error handling for searchbar and made searches get added to search history immediately with refetch.

--- MovieSearchNative/components/DisplayMovies.tsx
+++ MovieSearchNative/components/DisplayMovies.tsx
@@ -48,8 +48,6 @@ export default function DisplayMovies() {
     }
   }
 
-  console.log(sort)
-
   const { loading, error, data } = useQuery(setQuery(), {
     variables: {
       where: {
--- MovieSearchNative/components/SearchBar.tsx
+++ MovieSearchNative/components/SearchBar.tsx
@@ -1,4 +1,5 @@
 import {
+  Text,
   Keyboard,
   SafeAreaView,
   TextInput,
@@ -8,13 +9,62 @@ import React, { useState } from "react";
 import { Octicons } from "@expo/vector-icons";
 import { titleSearchedFor } from "../utils/stateManagement";
 import { styles } from "../styles/SearchBar";
+import { GET_SEARCHES } from "../queries/getSearches";
+import { useMutation } from "@apollo/client";
+import { CREATE_SEARCHES } from "../queries/createSearches";
+import { PAGE_OPTIONS } from "../utils/enum";
 
 export default function SearchBar() {
   const [search, setSearch] = useState("");
+  const d = new Date();
 
-  function addSearch() {
-    titleSearchedFor(search);
+  // String of invalid characters
+  const format = /[!@#$%^&*()_+\-=\[\]{};':"\\|<>\/]+/;
+
+  // Createa search
+  const [addSearch, { loading, error }] = useMutation(CREATE_SEARCHES, {
+    refetchQueries: [
+      {
+        query: GET_SEARCHES,
+        variables: {
+          options: {
+            offset: 0,
+            limit: PAGE_OPTIONS.SEARCHES_SIZE,
+            sort: {
+              created: "DESC",
+            },
+          },
+        },
+      },
+      "getSearches",
+    ],
+  });
+
+  // Adds the word searched for to the database
+  function addToSearchLog() {
+    titleSearchedFor(search.trim());
+    if (search.trim()) {
+      addSearch({
+        variables: {
+          title: search,
+          created: d.toISOString(),
+        },
+      });
+    }
   }
+
+  const onSubmit = () => {
+    if (!format.test(search)) {
+      addToSearchLog();
+    } else {
+      alert(
+        "No special characters allowed! Please try again."
+      );
+    }
+  };
+
+  if (loading) return <Text>Saving search ...</Text>;
+  if (error) return <Text>Could not save search ...</Text>;
 
   return (
     <SafeAreaView style={[styles.searchBarContainer]}>
@@ -34,7 +84,7 @@ export default function SearchBar() {
           size={35}
           color="white"
           title="Search"
-          onPress={addSearch}
+          onPress={onSubmit}
         />
       </TouchableWithoutFeedback>
     </SafeAreaView>
